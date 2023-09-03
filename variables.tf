df3b64b3082813664a42326edaf8e4d6a94b427b
Karapetyan, Aram <aram@dasmeta.com>
2023-09-03 14:59:23 +0400

    feat(DMVP-2679): add test variables

--- variables.tf
+++ variables.tf
@@ -1,1 +1,17 @@
-# empty
+variable "first-string" {
+  type        = string
+  default     = "string-value"
+  description = "String variable to be tested in other modules"
+}
+
+variable "second-bool" {
+  type        = bool
+  default     = true
+  description = "Boolean variable to be tested in other modules"
+}
+
+variable "third-number" {
+  type        = number
+  default     = 345678
+  description = "Numeric variable to be tested in other modules"
+}
